5a3ead12d483a43096f18c7aa0408fc15cb78490
RadhaDharani <164617685+RadhaDharani@users.noreply.github.com>
2024-06-02 16:50:30 +0530

    Add files via upload

---
+++ Day-19 Flip Flops/JKFlipFlop.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.06.2024 16:28:02
+// Design Name: 
+// Module Name: JKFlipFlop
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module JKFlipFlop(
+
+    );
+endmodule
